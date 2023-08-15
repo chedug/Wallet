c0cc6560e02f5ab4b5e9d2afa1ca1a0254f729e9
chedug <nuralyhug@gmail.com>
2023-08-15 14:24:25 +0200

    feat: tokens are shown when user is registered.

--- testproject/registration/views.py
+++ testproject/registration/views.py
@@ -2,7 +2,9 @@
 Views for Registration app
 """
 from django.contrib.auth.models import User
+from rest_framework import status
 from rest_framework.generics import CreateAPIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .serializers import UserRegistrationSerializer
 
@@ -14,3 +16,17 @@ class UserRegistrationView(CreateAPIView):
 
     queryset = User.objects.all()
     serializer_class = UserRegistrationSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+        When user is created, its tokens will be shown
+        """
+        username = request.data["username"]
+        response = super().create(request, *args, **kwargs)
+
+        if response.status_code == status.HTTP_201_CREATED:
+            user = User.objects.get(username=username)
+            refresh = RefreshToken.for_user(user)
+            response.data["access_token"] = str(refresh.access_token)
+            response.data["refresh_token"] = str(refresh)
+        return response
